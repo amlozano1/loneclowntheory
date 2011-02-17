7fdd12b7264a6f7e5ea45d8134b0987e828952dc
lockes5hadow <lockes5hadow@4bb3e9bc-af2e-49e9-dc86-fa669148392f>
2011-02-17 22:14:23 +0000

    Forgot to add inserts and rename column to entity from object

---  loneclowntheory --username brandon.t.andersen@gmail.com/src/loneclowntheory/createTable.sql
+++  loneclowntheory --username brandon.t.andersen@gmail.com/src/loneclowntheory/createTable.sql
@@ -25,27 +25,27 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `LoneClownTheory`.`acm` ;
 
 CREATE  TABLE IF NOT EXISTS `LoneClownTheory`.`acm` (
-  `Subject` VARCHAR(10) NOT NULL ,
-  `Object` VARCHAR(10) NOT NULL ,
-  `Granter` VARCHAR(10) NOT NULL ,
+  `subject` VARCHAR(10) NOT NULL ,
+  `entity` VARCHAR(10) NOT NULL ,
+  `granter` VARCHAR(10) NOT NULL ,
   `right` VARCHAR(1) NOT NULL ,
   `timestamp` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  PRIMARY KEY (`Subject`, `Object`) ,
-  INDEX `entity` (`Object` ASC) ,
-  INDEX `subject` (`Subject` ASC) ,
-  INDEX `granter` (`Granter` ASC) ,
+  PRIMARY KEY (`subject`, `entity`) ,
+  INDEX `entity` (`entity` ASC) ,
+  INDEX `subject` (`subject` ASC) ,
+  INDEX `granter` (`granter` ASC) ,
   CONSTRAINT `subject`
-    FOREIGN KEY (`Subject` )
+    FOREIGN KEY (`subject` )
     REFERENCES `LoneClownTheory`.`entityTable` (`entityName` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `entity`
-    FOREIGN KEY (`Object` )
+    FOREIGN KEY (`entity` )
     REFERENCES `LoneClownTheory`.`entityTable` (`entityName` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `granter`
-    FOREIGN KEY (`Granter` )
+    FOREIGN KEY (`granter` )
     REFERENCES `LoneClownTheory`.`entityTable` (`entityName` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -63,5 +63,14 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 SET AUTOCOMMIT=0;
 USE `LoneClownTheory`;
 INSERT INTO `LoneClownTheory`.`entityTable` (`entityID`, `entityName`, `subject_or_object`) VALUES (1, 'subject0', 1);
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `LoneClownTheory`.`acm`
+-- -----------------------------------------------------
+SET AUTOCOMMIT=0;
+USE `LoneClownTheory`;
+INSERT INTO `LoneClownTheory`.`acm` (`subject`, `entity`, `granter`, `right`, `timestamp`) VALUES ('subject0', 'subject0', 'subject0', 'o', 1);
 
 COMMIT;
